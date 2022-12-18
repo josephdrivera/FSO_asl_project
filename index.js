1a5f1bc1daf00e8c0f72835d0833bdcaa7dc72b2
Joseph D Rivera <josephdrivera15@gmail.com>
2022-12-18 12:18:15 -0500

    e-commerce

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
 const bodyParser = require("body-parser");
+
 const productRouter = require("./routes/Products");
 const variantsRouter = require("./routes/Variants");
 const imagesRouter = require("./routes/Images");
+const storefrontRouter = require("./routes/Storefront");
+const { index } = require("./controllers/Storefront");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(fileUpload());
+
 app.set("views", __dirname + "/templates/views");
 app.set("view engine", "twig");
 
-app.get("/", (req, res) => {
-    res.render("home");
-});
+app.get("/", index);
 
+
+app.use("/public", express.static("public"));
+
+app.use("/store", storefrontRouter);
 app.use("/products", productRouter);
 app.use("/variants", variantsRouter);
 app.use("/images", imagesRouter);
